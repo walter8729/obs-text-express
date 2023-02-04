50987801b46b483360019b7c2447be0c60c9c394
Romi Walter Miranda <walter8729@gmail.com>
2023-02-04 10:37:40 -0300

    update  index.js for open browser

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import app from "./app";
 app.listen(app.get("port"));
 console.log("Server on port", app.get("port"));
 
-
+//Copiado url, stackoverflow.
 let url = 'http://localhost' + ':' + app.get('port');
 let start = (process.platform == 'darwin'? 'open': process.platform == 'win32'? 'start': 'xdg-open');
 require('child_process').exec(start + ' ' + url);
